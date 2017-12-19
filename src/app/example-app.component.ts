77c12026dd92e50ff97a8448c206692a05d5c2eb
meierr14 <meierr14@students.zhaw.ch>
2017-12-19 18:58:43 +0100

    updated

--- src/app/example-app.component.ts
+++ src/app/example-app.component.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /* angular/path imports */
 import {Component} from '@angular/core';
 import * as path from "path-framework/app/path-framework/path";
@@ -56,65 +56,5 @@ export class ExampleAppComponent extends path.PathAppComponent {
     protected getHandlers() {
         return handler;
     }
-
-=======
-/* angular/path imports */
-import {Component} from '@angular/core';
-import * as path from "path-framework/app/path-framework/path";
-
-/* model imports */
-import {GuiModel} from './gui-model/guimodel';
-import * as handler from './gui-model/form/handlers'
-import * as beans from './gui-model/generated/forms'
-import {TranslationService} from "path-framework/app/path-framework/service/translation.service";
-import {ExampleTranslationService} from "./example-translation-service";
-
-@Component({
-    selector: 'path-application',
-    templateUrl: './../../node_modules/path-framework/app/path-framework/path-app.component.html',
-    providers: [path.PathService, { provide: TranslationService, useClass: ExampleTranslationService }]
-})
-export class ExampleAppComponent extends path.PathAppComponent {
-
-    private _appConfig = new GuiModel();
-
-    constructor(pathService: path.PathService, translationService: TranslationService) {
-        super(pathService, translationService);
-    }
-
-    protected getFrontendVersion():string {
-        return "0.2.7";
-    }
-
-    protected getStartPage():string {
-        return "mainmenu";
-    }
-
-    protected getOwnUserForm():string {
-        return "UserForm";
-    }
-
-    protected getGuiModel() {
-        if (this._appConfig != null) {
-            return this._appConfig.guiModel;
-        }
-        return null;
-    }
-
-    public getBackendUrl() {
-        if (window.location.hostname.indexOf("herokuapp") != -1) {
-            return location.protocol + "//re-friend-tracker-solution.herokuapp.com/services";
-        }
-        return "http://localhost:8080/services";
-    }
-    
-    protected getBeans() {
-        return beans;
-    }
-    
-    protected getHandlers() {
-        return handler;
-    }
-
->>>>>>> e9856c824fcda8b3e91db787261566391dab9d95
 }
+
